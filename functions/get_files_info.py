cd6673eda20bf9372b869cc0472d58593fb3b8ad
Karl Hans Laurits <karl@laurits.dev>
2025-12-07 16:08:07 +0200

    Initial commit

---
+++ functions/get_files_info.py
@@ -0,0 +1,5 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    path = os.path.join(working_directory, directory)
---
+++ main.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("Gemini API key not found. Please add it to .env file.")
+
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.5-flash"
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+
+    response = client.models.generate_content(model=model, contents=messages)
+
+    if response.usage_metadata is not None:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+    else:
+        raise RuntimeError("API request failed, please try again.")
+
+    if args.verbose:
+        print(f"User prompt: {response.text}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    else:
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
